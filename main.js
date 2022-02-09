fc94639661f6f1f18ec6e3a00c3e5f6017a72fad
Paul Reece <paulreece2@gmail.com>
2022-02-09 14:41:14 -0500

    functioning converter

--- main.js
+++ main.js
@@ -2,32 +2,27 @@ let url =
   "https://openexchangerates.org/api/latest.json?app_id=f6022ce66f5048338fb1896198a438e3&base=USD";
 
 let curr = document.getElementById("curDrop");
+let curr2 = document.getElementById("curDrop2");
 let display = document.querySelector("#display");
-
-// function rate(curr) {
-//   const idx = currencies.findIndex(function (name) {
-//     return currenc;
-//   });
-
-//   if (idx === -1) {
-//     return null;
-//   }
-
-//   return usStates[idx].abbreviation;
-// }
+let amount = document.getElementById("amount");
 
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
     for (let [key, value] of Object.entries(data.rates)) {
-      curr.innerHTML += `<option>${key} ${value}</option>`;
-      curr.addEventListener("change", function (e) {
-        let bob = Object;
-        display.innerHTML += bob;
+      curr.innerHTML += `<option id='${key}'>${key}</option>`;
+      curr2.innerHTML += `<option id='${key}'>${key}</option>`;
+
+      convert.addEventListener("click", function (e) {
+        let keyid = curr.options[curr.selectedIndex].value;
+        let keyed = data.rates[keyid];
+        let convertedUS = amount.value * keyed;
+        let keyids = curr2.options[curr2.selectedIndex].value;
+        let keyeds = data.rates[keyids];
+
+        let actualConv = convertedUS * keyeds;
+
+        display.innerHTML = actualConv.toFixed(2) + " " + keyids;
       });
     }
   });
-// .then(() => {
-//   for (let currency of currencies)
-
-// })
